bf1db4fb2ad81226d250fda82dc72dbfb33cdfcc
maxgunter99 <maxgunter99@gmail.com>
2019-07-16 16:52:13 -0500

    refatoring

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -14,33 +14,77 @@ def merge( arrA, arrB ):
 
 def merge_sort( arr ):
 
-    # TO-DO
-    # print( "ARRAY!" , arr )
-    # RETURNS [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+    # ARRAY
+    print( "Array" , arr )
 
-    if len( arr ) == 0:
-        return arr
-    pivot = arr[ 0 ]
-    pivots = [ x for x in arr if x == pivot ]
-    small = merge_sort( [ x for x in arr if x < pivot ] )
-    large = merge_sort( [ x for x in arr if x > pivot ] )
-    print( small + pivots + large )
+    # MIDDLE VALUE
+    pivot_point = len( arr ) // 2
 
-    # RETURNS [
-# 0]
-# [3]
-# [2, 3]
-# [2, 3, 4]
-# [7]
-# [6, 7]
-# [9]
-# [6, 7, 8, 9]
-# [4, 2, 3, 5, 8, 9, 6, 7]
-# [0, 1, 5, 8, 4, 2, 9, 6, 3, 7]
+    # ALL ITEMS LARGER THAN PIVOT_POINT
+    taller = arr[ pivot_point: ]
+    print( "Taller" , taller )
+    taller_pivot_point = len( taller ) // 2
+    print( "taller_pivot_point" , taller[taller_pivot_point] )
 
-            
+    # ALL ITEMS SMALLER THAN PIVOT_POINT
+    shorter = arr[ :pivot_point ]
+    print( "Shorter" , shorter )
+    shorter_pivot_point = len( shorter ) // 2
+    print( "shorter_pivot_point" , shorter[shorter_pivot_point] )
 
     return arr
+
+
+
+
+
+
+
+
+# # TO-DO: complete the helpe function below to merge 2 sorted arrays
+
+# def merge( arrA, arrB ):
+
+#     elements = len( arrA ) + len( arrB )
+#     merged_arr = [0] * elements
+
+#     # TO-DO
+
+
+#     return merged_arr
+
+# # TO-DO: implement the Merge Sort function below USING RECURSION
+
+# def merge_sort( arr ):
+
+#     if len( arr ) == 0:
+#         return arr
+
+#     # starting number, comparing pivot to every other number in set
+#     pivot = arr
+
+#     pivots = [ x for x in arr if x == pivot ]
+
+#     # decide if selected number in array is smaller than the pivot
+#     shorter = merge_sort( [ x for x in arr if x < pivot ] )
+
+#     # decide if selected number in array is larger than the pivot point
+#     taller = merge_sort( [ x for x in arr if x > pivot ] )
+
+#     # add all together, smallest to largest
+#     arr = shorter + pivots + taller
+
+#     print( arr )      
+
+#     return arr
+
+
+
+
+
+
+
+
 # STRETCH: implement an in-place merge sort algorithm
 # def merge_in_place(arr, start, mid, end):
 #     # TO-DO
